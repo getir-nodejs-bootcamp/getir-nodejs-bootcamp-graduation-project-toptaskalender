da58c51aaeba8c8af5375d8af6f438f92e38c283
toptaskalender <toptaskalender@gmail.com>
2021-12-26 16:24:49 +0300

    Create getRecordsSchema to control request payload, and export it

---
+++ src/validations/record/get-records.js
@@ -0,0 +1,35 @@
+const Joi         = require('joi');
+const {
+  joiErrorHandler
+}                 = require('../../utils/functions');
+
+const getRecordsSchema = Joi.object({
+  startDate: Joi
+    .date()
+    .iso()
+    .required()
+    .error(joiErrorHandler),
+
+  endDate: Joi
+    .date()
+    .iso()
+    .required()
+    .error(joiErrorHandler),
+
+  minCount: Joi
+    .number()
+    .min(0)
+    .strict()
+    .required()
+    .error(joiErrorHandler),
+
+  maxCount: Joi
+    .number()
+    .min(Joi.ref('minCount'))
+    .strict()
+    .required()
+    .error(joiErrorHandler),
+
+});
+
+module.exports = getRecordsSchema;
