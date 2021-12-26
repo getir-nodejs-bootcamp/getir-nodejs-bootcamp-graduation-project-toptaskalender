3cfcc4ed9ae4f8bbcbf5492c7c6833829114e24c
toptaskalender <toptaskalender@gmail.com>
2021-12-26 16:08:06 +0300

    Create validateAgainst middleware function to validate req payloads, and export it

---
+++ src/middlewares/validation/validate-against.js
@@ -0,0 +1,21 @@
+const { AppError } = require('../../utils/classes');
+
+function validateAgainst(schema) {
+  return async (req, res, next) => {
+    const { body: payload } = req;
+
+    const { error } = schema.validate(payload);
+
+    console.log('validateAgainst error => ', error);
+    
+    if (error) {
+      const message = error.details[0].message;
+
+      return next(new AppError(404, message));
+    }
+
+    next();
+  }
+}
+
+module.exports = validateAgainst;
