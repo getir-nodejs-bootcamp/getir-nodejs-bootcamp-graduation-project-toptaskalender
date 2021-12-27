e5617806f468b3695249265049b351c5e3f36d93
toptaskalender <toptaskalender@gmail.com>
2021-12-27 12:58:51 +0300

    Implement morgan

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express             = require('express');
+const morgan              = require('morgan');
+const {
+  requestLogStream
+}                         = require('./logs');
 const { 
   invalidEndpointHandler,
   errorHandler
 }                         = require('./middlewares');
 const { recordsRouter }   = require('./routes');
+
 const app                 = express();
 
 app.use(express.json());
+app.use(morgan('combined', { stream: requestLogStream }));
+app.use(morgan('dev'));
 
 app.use('/records', recordsRouter);
 
