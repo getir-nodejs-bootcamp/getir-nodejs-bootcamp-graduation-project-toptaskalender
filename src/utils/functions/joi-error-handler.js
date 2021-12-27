d9d07bc6f9901603db1ecec3103496f1a1af25ec
toptaskalender <toptaskalender@gmail.com>
2021-12-27 12:04:28 +0300

    Update joiErrorHandler to handle 'date.base' error type

--- src/utils/functions/joi-error-handler.js
+++ src/utils/functions/joi-error-handler.js
@@ -6,23 +6,26 @@ function joiErrorHandler(errors) {
     
     switch (err.code) {
       case 'any.required':
-        err.message = `${field.label} is a required field.`
-        break
+        err.message = `${field.label} is a required field.`;
+        break;
 
+      case 'date.base':
+        err.message = `${field.label} must be valid date.`;
+        break;
       case 'date.format':
-        err.message = `${field.label} format must be in the shape of (YYYY-MM-DD).`
-        break
+        err.message = `${field.label} must contain a date in a 'YYYY-MM-DD' format.`;
+        break;
 
       case 'number.base':
-        err.message = `${field.label} must be a type of number.`
-        break
+        err.message = `${field.label} must be a type of number.`;
+        break;
       case 'number.min':
-        err.message = `${field.label} must be greater than or equal to ${ field.limit.key || field.limit}.`
-        break
+        err.message = `${field.label} must be greater than or equal to ${ field.limit.key || field.limit}.`;
+        break;
     }
   })
 
   return errors;
 }
 
-module.exports = joiErrorHandler
+module.exports = joiErrorHandler;
