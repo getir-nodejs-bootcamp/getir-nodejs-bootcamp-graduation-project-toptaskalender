91871198e36ae541f59321e42b84216fbbea621f
toptaskalender <toptaskalender@gmail.com>
2021-12-27 13:10:13 +0300

    Install helmet, and implement it

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express             = require('express');
 const morgan              = require('morgan');
+const helmet              = require('helmet');
 const {
   requestLogStream
 }                         = require('./logs');
@@ -11,6 +12,7 @@ const { recordsRouter }   = require('./routes');
 
 const app                 = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(morgan('combined', { stream: requestLogStream }));
 app.use(morgan('dev'));
