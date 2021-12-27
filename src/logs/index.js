c5e6948377f9eae01f02aaeb946428ff00afed04
toptaskalender <toptaskalender@gmail.com>
2021-12-27 19:01:20 +0300

    Add comments

--- src/logs/index.js
+++ src/logs/index.js
@@ -1,8 +1,9 @@
 const fs    = require('fs');
 const path  = require('path');
 
+// Create a write stream in append mode
 const requestLogStream = fs.createWriteStream(
-  path.join(__dirname, 'request.log'),
+  path.join(__dirname, 'requests.log'),
   { flags: 'a' }
 );
 
