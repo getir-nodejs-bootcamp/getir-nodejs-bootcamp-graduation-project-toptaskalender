1212908e1d41766cb978d4789dc31962ac136395
toptaskalender <toptaskalender@gmail.com>
2021-12-27 18:17:23 +0300

    Add comments

--- src/middlewares/validation/validate-against.js
+++ src/middlewares/validation/validate-against.js
@@ -1,5 +1,10 @@
 const { AppError } = require('../../utils/classes');
 
+/**
+ * @desc Higher-order function that takes an schema and returns an asynchronous middleware function
+ * @param {object} schema The validation schema by which request data will be validated
+ * @returns {function} Asynchronous middleware function
+ */
 function validateAgainst(schema) {
   return async (req, res, next) => {
     const { body: payload } = req;
@@ -11,9 +16,11 @@ function validateAgainst(schema) {
     if (error) {
       const message = error.details[0].message;
 
+      // Delegate the error to the global error handler middleware
       return next(new AppError(400, message));
     }
 
+    // If there is no error, continue with next middleware function
     next();
   }
 }
