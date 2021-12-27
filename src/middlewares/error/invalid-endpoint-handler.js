3c7b1f84cfe30b0868b1acdb15fa8f802ef42ddb
toptaskalender <toptaskalender@gmail.com>
2021-12-27 18:15:51 +0300

    Add comments

--- src/middlewares/error/invalid-endpoint-handler.js
+++ src/middlewares/error/invalid-endpoint-handler.js
@@ -1,5 +1,11 @@
 const { AppError } = require('../../utils/classes');
 
+/**
+ * @desc A middleware that handles requests to unknown endpoints by delegating them to the global error middleware
+ * @param {object} req Request object
+ * @param {object} res Response object
+ * @param {function} next Next middleware function
+ */
 async function invalidEndpointHandler(req, res, next) {
   next(
     new AppError(
