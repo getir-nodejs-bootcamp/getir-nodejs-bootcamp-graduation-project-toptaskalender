c6840e50403c2db874b34e514b33347aeae08021
toptaskalender <toptaskalender@gmail.com>
2021-12-27 17:32:23 +0300

    Add comments

--- src/app.js
+++ src/app.js
@@ -16,12 +16,15 @@ const app                 = express();
 app.use(cors({ methods: '*', origin: '*' }));
 app.use(helmet());
 app.use(express.json());
+// Log requests into requests.log file via write stream with the help of Morgan
 app.use(morgan('combined', { stream: requestLogStream }));
 app.use(morgan('dev'));
 
 app.use('/api/v1/records', recordsRouter);
 
+// Handle requests to unknown endpoints by delegating them to the global error handler middleware
 app.all('*', invalidEndpointHandler);
+// Use the global error handler middleware
 app.use(errorHandler);
 
 module.exports = app;
