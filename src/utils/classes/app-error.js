21ebad389c9d0d43a789b90ed6fdb6c4ef4a1aa6
toptaskalender <toptaskalender@gmail.com>
2021-12-27 18:20:19 +0300

    Add comments

--- src/utils/classes/app-error.js
+++ src/utils/classes/app-error.js
@@ -1,9 +1,14 @@
+// A extended error class to handle operational errors that occur in our program
 class AppError extends Error {
   constructor(statusCode, message) {
+    // Set the incoming message to the message property of the parent 'Error' class
     super(message);
 
+    // Mark the errors that are created by this class as operational errors
     this.isOperational  = true;
+    // Define the error's status code using incoming statusCode parameter
     this.statusCode     = statusCode;
+    // Decide the response's code key by simply looking the status code of the request.
     this.code           = String(statusCode).startsWith('4') ? '1' : '2';
   }
 }
