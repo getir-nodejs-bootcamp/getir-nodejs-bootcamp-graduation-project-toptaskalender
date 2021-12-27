7da58767dd9bf28a24961f399ce3f5c266666c11
toptaskalender <toptaskalender@gmail.com>
2021-12-27 11:48:16 +0300

    Change res payload, and change the error that is passed

--- src/controllers/records.js
+++ src/controllers/records.js
@@ -1,19 +1,19 @@
 const { recordService } = require('../services');
-const { AppError }      = require('../utils/classes');
 
 async function getRecords(req, res, next) {
   try {
     const { body: filter }  = req;
     const records           = await recordService.findByDateAndTotalCount(filter);
 
+    console.log('records length => ', records.length);
+
     res.status(200).json({
-      results: records.length,
       code: 0,
       msg: 'success',
       records
     });
   } catch (e) {
-    next(new AppError(500, 'Something went wrong.'));
+    next(e);
   }
 }
 
