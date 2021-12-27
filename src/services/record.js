b4f6cbafbf0c35480bf181795fb73761c8a2aa08
toptaskalender <toptaskalender@gmail.com>
2021-12-27 18:54:44 +0300

    Add comments

--- src/services/record.js
+++ src/services/record.js
@@ -4,7 +4,17 @@ class RecordService {
   constructor(model) {
     this.model = model;
   }
-  
+
+  /**
+   * @desc An asynchronous function that talks to 'records' collection in the database
+   * to filter documents using MongoDB's aggregate framework. The filtering steps are;
+   *  1 - `$match` to filter documents by createdAt field and used as a first stage to optimize the query performance,
+   *  2 - `$addFields` to add a field that contains total count value calculated from counts array items,
+   *  3 - `$match` to filter documents by totalCount value,
+   *  4 - `$project` to select required fields.
+   * @param {object} object An object containing filter keys
+   * @returns Mongoose's aggregate object 
+   */
   async findByDateAndTotalCount({ startDate, endDate, minCount, maxCount }) {
     return this.model.aggregate([
       {
