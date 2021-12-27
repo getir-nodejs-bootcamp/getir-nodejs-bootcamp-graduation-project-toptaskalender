a8a67cd7646a6c1d289dc6c34ce44ce674640bcc
toptaskalender <toptaskalender@gmail.com>
2021-12-27 18:18:16 +0300

    Add comments

--- src/routes/records.js
+++ src/routes/records.js
@@ -9,6 +9,7 @@ const { getRecords }  = require('../controllers/records');
 
 router.post(
   '/',
+  // Validate request data with the help of Joi schema
   validateAgainst(getRecordsSchema),
   getRecords
 );
